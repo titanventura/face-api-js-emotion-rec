a2da1bbf35491bea4c5ea7dbd11c3e2e98372248
gokulkanna <gokulkanna.19is@kct.ac.in>
2022-03-01 11:44:31 +0530

    mtqq latest one changes

---
+++ config.js
@@ -0,0 +1,19 @@
+// host = '172.16.153.122';	// hostname or IP address
+host = 'mqtt.iqube.io';	// hostname or IP address
+// host = '172.16.153.110';	// hostname or IP address
+port = 443;
+topic = 'srinath';		// topic to subscribe to
+useTLS = true;
+username = 'iqube';
+password = 'iQube@2019..';
+// username = "jjolie";
+// password = "aa";
+
+// path as in "scheme:[//[user:password@]host[:port]][/]path[?query][#fragment]"
+//    defaults to "/mqtt"
+//    may include query and fragment
+//
+// path = "/mqtt";
+// path = "/data/cloud?device=12345";
+
+cleansession = true;
